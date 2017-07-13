f859b5e4145cf82aa0571db81d6fef30e5637c6b
Constantin Pahl <constantin.pahl@gmail.com>
2017-07-13 17:59:14 +0200

    index done and deployed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [ :index, :show, :new, :create ] do
+    resources :doses, only: [ :new, :create, :delete ]
+  end
 end
+
+
+# A user can see the list of all cocktails
+# GET "cocktails"
+# A user can see the details of a given cocktail, with the ingredient list
+# GET "cocktails/42"
+# A user can create a new cocktail.
+# GET "cocktails/new"
+# POST "cocktails"
+# A user can add a new dose (ingredient/description pair) on an existing cocktail.
+# GET "cocktails/42/doses/new"
+# POST "cocktails/42/doses"
+# A user can delete a dose on an existing cocktail
+# DELETE "doses/25"
+# And now think. Do we need an IngredientsController?
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
